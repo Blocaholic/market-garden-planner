fec1d734369f3ae184d2061e916022fb55d2bc18
Blocaholic <tobias@rocklands.de>
2022-04-20 11:28:21 +0200

    Refactor code modules

---
+++ Data.mjs
@@ -0,0 +1,67 @@
+import Utils from './Utils.mjs';
+
+const formatBoxes = csv => {
+  const arr = Utils.parseCSV(csv);
+  const head = arr[0].slice(1);
+  const boxes = new Array(arr.length - 1).fill().map(Object);
+  arr.slice(1).forEach((box, i) => {
+    boxes[i].datum = Utils.stringToDate(box[0]);
+    boxes[i].ingredients = {};
+    box.slice(1).forEach((quantity, j) => {
+      if (quantity) boxes[i].ingredients[head[j]] = Utils.commaToDot(quantity);
+    });
+  });
+  return Utils.deepFreeze(boxes);
+};
+
+const formatVeggies = csv => {
+  const arr = Utils.parseCSV(csv);
+  const kulturen = {};
+  arr[0].slice(1).forEach(id => (kulturen[id] = {}));
+  arr
+    .slice(1)
+    .forEach(row =>
+      row
+        .slice(1)
+        .forEach((element, i) => (kulturen[arr[0][i + 1]][row[0]] = element))
+    );
+  Object.entries(kulturen).forEach(([, kultur]) => {
+    kultur.fullName = `${kultur.Kulturname} ${kultur.Sortenname}`;
+    kultur.Anzuchtquote = Utils.commaToDot(kultur.Anzuchtquote || '1');
+    kultur.Feldquote = Utils.commaToDot(kultur.Feldquote || '1');
+    kultur['Erntemenge pro Ernte'] = Number(
+      Utils.commaToDot(kultur['Erntemenge pro Ernte'])
+    );
+    if (kultur['Erntemenge pro Ernte'] === 0)
+      throw 'Erwartete Erntemenge darf nicht Null sein!';
+    kultur['Erntehäufigkeit pro Pflanze'] = Number(
+      kultur['Erntehäufigkeit pro Pflanze']
+    );
+    kultur['Kulturdauer am Beet'] = Number(kultur['Kulturdauer am Beet']);
+    kultur.Anzuchtzeit = Number(kultur.Anzuchtzeit);
+    kultur.Erntezeittoleranz = Number(kultur.Erntezeittoleranz);
+    kultur.Ernteintervall = Number(kultur.Ernteintervall);
+    kultur.Ernteintervall =
+      kultur.Ernteintervall <= 7 && kultur.Ernteintervall > 0
+        ? 7
+        : Math.round(kultur.Ernteintervall / 7) * 7;
+    kultur.Quickpot = Number(kultur.Quickpot);
+    kultur.Vorziehen = kultur.Vorziehen === 'ja';
+  });
+  return Utils.deepFreeze(kulturen);
+};
+
+const getVeggies = () =>
+  Utils.readFile('./data/sortensteckbriefe.csv')
+    .then(formatVeggies)
+    .catch(console.error);
+
+const getBoxes = () =>
+  Utils.readFile('./data/kistenplanung.csv')
+    .then(formatBoxes)
+    .catch(console.error);
+
+export default Object.freeze({
+  getVeggies,
+  getBoxes,
+});
--- planner-script.mjs
+++ planner-script.mjs
@@ -1,66 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 import Utils from './Utils.mjs';
-import View from './View.mjs';
-
-const formatBoxes = csv => {
-  const arr = Utils.parseCSV(csv);
-  const head = arr[0].slice(1);
-  const boxes = new Array(arr.length - 1).fill().map(Object);
-  arr.slice(1).forEach((box, i) => {
-    boxes[i].datum = Utils.stringToDate(box[0]);
-    boxes[i].ingredients = {};
-    box.slice(1).forEach((quantity, j) => {
-      if (quantity) boxes[i].ingredients[head[j]] = Utils.commaToDot(quantity);
-    });
-  });
-  return Utils.deepFreeze(boxes);
-};
-
-const formatVeggies = csv => {
-  const arr = Utils.parseCSV(csv);
-  const kulturen = {};
-  arr[0].slice(1).forEach(id => (kulturen[id] = {}));
-  arr
-    .slice(1)
-    .forEach(row =>
-      row
-        .slice(1)
-        .forEach((element, i) => (kulturen[arr[0][i + 1]][row[0]] = element))
-    );
-  Object.entries(kulturen).forEach(([, kultur]) => {
-    kultur.fullName = `${kultur.Kulturname} ${kultur.Sortenname}`;
-    kultur.Anzuchtquote = Utils.commaToDot(kultur.Anzuchtquote || '1');
-    kultur.Feldquote = Utils.commaToDot(kultur.Feldquote || '1');
-    kultur['Erntemenge pro Ernte'] = Number(
-      Utils.commaToDot(kultur['Erntemenge pro Ernte'])
-    );
-    if (kultur['Erntemenge pro Ernte'] === 0)
-      throw 'Erwartete Erntemenge darf nicht Null sein!';
-    kultur['Erntehäufigkeit pro Pflanze'] = Number(
-      kultur['Erntehäufigkeit pro Pflanze']
-    );
-    kultur['Kulturdauer am Beet'] = Number(kultur['Kulturdauer am Beet']);
-    kultur.Anzuchtzeit = Number(kultur.Anzuchtzeit);
-    kultur.Erntezeittoleranz = Number(kultur.Erntezeittoleranz);
-    kultur.Ernteintervall = Number(kultur.Ernteintervall);
-    kultur.Ernteintervall =
-      kultur.Ernteintervall <= 7 && kultur.Ernteintervall > 0
-        ? 7
-        : Math.round(kultur.Ernteintervall / 7) * 7;
-    kultur.Quickpot = Number(kultur.Quickpot);
-    kultur.Vorziehen = kultur.Vorziehen === 'ja';
-  });
-  return Utils.deepFreeze(kulturen);
-};
-
-const veggiesPromise = Utils.readFile('./data/sortensteckbriefe.csv')
-  .then(formatVeggies)
-  .catch(console.error);
-
-const boxesPromise = Utils.readFile('./data/kistenplanung.csv')
-  .then(formatBoxes)
-  .catch(console.error);
+import Data from './Data.mjs';
 
 const plant = (veggies, boxes, numberOfBoxes) => {
   const plantSets = [];
@@ -163,6 +104,9 @@ const plant = (veggies, boxes, numberOfBoxes) => {
   });
   return plantSets;
 };
+
+const veggiesPromise = Data.getVeggies();
+const boxesPromise = Data.getBoxes();
 
 Promise.all([veggiesPromise, boxesPromise]).then(([veggies, boxes]) => {
   const plants = plant(veggies, boxes, 174);
