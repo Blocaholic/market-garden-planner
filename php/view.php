4bce3e7451e052a209cb81182ad7cda1ce093685
Blocaholic <tobias@rocklands.de>
2023-01-29 18:55:56 +0100

    Restructure for php content management

--- php/view.php
+++ php/view.php
@@ -4,29 +4,16 @@ function renderContent($target, $error) {
   if(!$_SESSION['isValidUser']) {
     return $error.'<h2>Login-Formular</h2>';
   }
-  if($target == 'boxes') {
-    return '<h2>Kistenplanung</h2>';
-  }
-  if($target == 'reports') {
-    return '<h2>Auswertungen</h2><img src="https://reinwiese.de/images/logo.svg">';
-  }
-  if($target == 'home') {
-    return '<h2>Welcome home</h2>';
+  if(file_exists('../templates/content/'.$target.'.html')) {
+    return file_get_contents("../templates/content/".$target.".html");
   }
   return 'Page not found!';
-  $content = '<h2>Überschrift</h2>
-  <h3>Überschrift</h3>
-  <h4>Überschrift</h4>
-  <p>Text</p>';
 }
 
 function renderNav($target, $user) {
   $nav = ($user["isValid"]) ?
   '<li class="header__mainNavLi header__mainNavLi--active">
     <a href="?target=boxes">Kisten</a>
-  </li>
-  <li class="header__mainNavLi">
-    <a href="?target=reports">Reports</a>
   </li>
   <li class="header__mainNavLi">
     <a href="?target=logout">Logout</a>
@@ -37,14 +24,25 @@ function renderNav($target, $user) {
   return $nav;
 }
 
+function renderScript($target, $user) {
+  if ($target == 'boxes' && $user["isValid"]) {
+    return '<script src="js/boxes.mjs" type="module"></script>';
+  }
+  else {
+    return '';
+  }
+}
+
 function renderPage($target, $error, $user) {
   
   $page = file_get_contents("../templates/page.html");
   $nav = renderNav($target, $user);
   $content = renderContent($target, $error);
+  $script = renderScript($target, $user);
 
   $page = preg_replace("/\[\%nav\%\]/", $nav, $page);
   $page = preg_replace("/\[\%content\%\]/", $content, $page);
+  $page = preg_replace("/\[\%script\%\]/", $script, $page);
   
   return $page;
 }
