ef50cb5fe5628f2d6addf6289ea6d8f0754b716f
Blocaholic <tobias@rocklands.de>
2023-03-13 00:15:30 +0100

    Report-1: fetch Sowings from Database

--- www/js/report-1.mjs
+++ www/js/report-1.mjs
@@ -1,1 +1,20 @@
-console.log('Welcome to Report 1');
+import {Sowing, Veggie, Crop} from './Datatypes.mjs';
+import {fetchJson} from './Utils.mjs';
+
+const sowings = await fetchJson(
+  'https://marketgardenapi.reinwiese.de/sowings.php'
+).then(data =>
+  data.map(
+    sowing =>
+      new Sowing({
+        veggie: new Veggie(sowing.veggie),
+        sowingDate: new Date(sowing.sowingDate),
+        seedAmount: sowing.seedAmount,
+        crops: sowing.crops.map(
+          crop => new Crop(new Date(crop.date), crop.amount, crop.salesChannel)
+        ),
+      })
+  )
+);
+
+console.log(sowings);
