12edc2fd51243243a2757b750d2a36f1e2612fb4
Blocaholic <tobias@rocklands.de>
2023-03-14 09:49:19 +0100

    Report-1: Render data to table rows

--- www/js/Report-1View.mjs
+++ www/js/Report-1View.mjs
@@ -10,9 +10,30 @@ const renderWorkSteps = workSteps => {
     if (workStep.workStep === 'clearBed') return 'Beet roden';
     return '';
   };
-  const rows = workSteps
-    .map(workStep => {
-      const row = `<tr>
+
+  const sum = {
+    quickpots: {
+      15: 0,
+      77: 0,
+      84: 0,
+      144: 0,
+      all: 0,
+    },
+    bedLength: 0,
+  };
+
+  const rows = workSteps.reduce((rows, workStep) => {
+    sum.quickpots[15] += workStep.quickpots[15] || 0;
+    sum.quickpots[77] += workStep.quickpots[77] || 0;
+    sum.quickpots[84] += workStep.quickpots[84] || 0;
+    sum.quickpots[144] += workStep.quickpots[144] || 0;
+    sum.quickpots.all += workStep.quickpots[15] || 0;
+    sum.quickpots.all += workStep.quickpots[77] || 0;
+    sum.quickpots.all += workStep.quickpots[84] || 0;
+    sum.quickpots.all += workStep.quickpots[144] || 0;
+    sum.bedLength += workStep.bedLength;
+    sum.bedLength = Math.round(sum.bedLength * 100) / 100;
+    const row = `<tr>
       <td>${dateToString(workStep.date)}</td>
       <td>${translateWorkStep(workStep)}</td>
       <td>${workStep.veggie.id} ${workStep.veggie.fullName}</td>
@@ -22,16 +43,16 @@ const renderWorkSteps = workSteps => {
       <td>${workStep.quickpots[84] || ''}</td>
       <td>${workStep.quickpots[144] || ''}</td>
       <td>${workStep.bedLength}</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td>${sum.quickpots[15]}</td>
+      <td>${sum.quickpots[77]}</td>
+      <td>${sum.quickpots[84]}</td>
+      <td>${sum.quickpots[144]}</td>
+      <td>${sum.quickpots.all}</td>
+      <td>${sum.bedLength}</td>
       </tr>`;
-      return row;
-    })
-    .join('');
+    return rows + row;
+  }, '');
+
   $('report-1__table').querySelector('tbody:first-of-type').innerHTML = rows;
 };
 
