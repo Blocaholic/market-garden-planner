3e6f3af07140032566fea5e7b3d35dd3b16d1031
Blocaholic <tobias@rocklands.de>
2023-05-14 07:04:38 +0200

    extract function checkSignup()

--- php/main.php
+++ php/main.php
@@ -5,6 +5,34 @@ function isValidUser() {
   return true;
 }
 
+function checkSignup() {
+  if (empty($_POST['name'])) {
+    die('Es muss ein Benutzername angegeben werden!');
+  }
+
+  if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    die('Es muss eine g&uuml;ltige E-Mail-Adresse angegeben werden!');
+  }
+
+  if (strlen($_POST['password']) < 8) {
+    die('Das Passwort muss mindestens 8 Zeichen lang sein!');
+  }
+
+  if (!preg_match('/[a-z]/i', $_POST['password'])) {
+    die('Das Passwort muss mindestens einen Buchstaben enthalten!');
+  }
+
+  if (!preg_match('/[0-9]/', $_POST['password'])) {
+    die('Das Passwort muss mindestens eine Ziffer enthalten!');
+  }
+
+  if ($_POST['password'] !== $_POST['password2']) {
+    die('Die Passw&ouml;rter stimmen nicht überein!');
+  }
+
+  return 'home';
+}
+
 function main() {
   session_start();
   $error = '';
@@ -13,31 +41,7 @@ function main() {
   $_SESSION['isValidUser'] = $_SESSION['isValidUser'] ?? false;
 
   if ($request == 'checkSignup') {
-    if (empty($_POST['name'])) {
-      die('Es muss ein Benutzername angegeben werden!');
-    }
-
-    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-      die('Es muss eine g&uuml;ltige E-Mail-Adresse angegeben werden!');
-    }
-
-    if (strlen($_POST['password']) < 8) {
-      die('Das Passwort muss mindestens 8 Zeichen lang sein!');
-    }
-
-    if (!preg_match('/[a-z]/i', $_POST['password'])) {
-      die('Das Passwort muss mindestens einen Buchstaben enthalten!');
-    }
-
-    if (!preg_match('/[0-9]/', $_POST['password'])) {
-      die('Das Passwort muss mindestens eine Ziffer enthalten!');
-    }
-
-    if ($_POST['password'] !== $_POST['password2']) {
-      die('Die Passw&ouml;rter stimmen nicht überein!');
-    }
-
-    print_r($_POST);
+    $request = checkSignup();
   }
 
   if ($request == 'login') {
