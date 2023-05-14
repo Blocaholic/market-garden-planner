cf84cd595acca41b6900c6d3bb9152e25d0b579c
Blocaholic <tobias@rocklands.de>
2023-05-14 10:29:11 +0200

    remove 'use User'

--- php/main.php
+++ php/main.php
@@ -2,8 +2,6 @@
 require_once 'view.php';
 require_once 'User.php';
 
-use User;
-
 function main() {
   session_start();
   $error = '';
@@ -12,11 +10,11 @@ function main() {
   $_SESSION['isValidUser'] = $_SESSION['isValidUser'] ?? false;
 
   if ($request == 'checkSignup') {
-    $request = User\checkSignup();
+    $request = \User\checkSignup();
   }
 
   if ($request == 'login') {
-    $_SESSION['isValidUser'] = User\isValidUser();
+    $_SESSION['isValidUser'] = \User\isValidUser();
     if (!$_SESSION['isValidUser']) {
       $error .= 'Fehler: Benutzername oder Passwort falsch!<br>';
     } else {
