7e19697b5725fb241fc5e4eedd0fece6f5ea9106
Blocaholic <tobias@rocklands.de>
2024-01-10 11:19:14 +0100

    order seedRequirements by culture at "report-2"

--- www/js/Report-2View.mjs
+++ www/js/Report-2View.mjs
@@ -1,12 +1,18 @@
 const $ = id => document.getElementById(id);
 
 const render = seedRequirement => {
-  const rows = seedRequirement.reduce(
-    (rows, seed) =>
-      rows +
-      `<tr><td>${seed.culture}</td><td>${seed.variety}</td><td>${seed.seedAmount}</td></tr>`,
-    ''
-  );
+  const rows = seedRequirement
+    .sort((a, b) => {
+      const cultureA = a.culture.toLowerCase();
+      const cultureB = b.culture.toLowerCase();
+      return cultureA < cultureB ? -1 : cultureA > cultureB ? 1 : 0;
+    })
+    .reduce(
+      (rows, seed) =>
+        rows +
+        `<tr><td>${seed.culture}</td><td>${seed.variety}</td><td>${seed.seedAmount}</td></tr>`,
+      ''
+    );
   $('report-2__table').querySelector('tbody:first-of-type').innerHTML = rows;
 };
 
